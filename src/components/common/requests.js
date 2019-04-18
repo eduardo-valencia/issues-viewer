765774dc7709180ade2430545a815e18f4fca7ec
Eduardo Valencia <8025918@gmail.com>
2019-04-17 19:34:28 -0700

    Add requests file that uses token.

--- src/components/common/requests.js
+++ src/components/common/requests.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
+import token from '../../settings'
 
 export default function getData(query, useData) {
   const instance = axios.create({
     headers: {
-      Authorization: 'bearer 50dd1a06d10098a978467c3a11949d6dfdf71c2f'
+      Authorization: `bearer ${token}`
     }
   })
   instance
     .post('https://api.github.com/graphql', { query: query })
     .then(response => {
-      console.log(response.data)
       useData(response.data)
     })
     .catch(error => {
--- src/settings.js
+++ src/settings.js
@@ -1,2 +1,2 @@
-const personalAccessToken = '50dd1a06d10098a978467c3a11949d6dfdf71c2f'
+const personalAccessToken = 'paste token here'
 export default personalAccessToken
